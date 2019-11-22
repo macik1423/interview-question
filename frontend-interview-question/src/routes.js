b0c4b3053159964709c9616f383c43ff1c5abcfc
Maciej Kowalik <m.kowalik@stat.gov.pl>
2019-11-22 15:21:54 +0100

    dodanie plikow

---
+++ frontend-interview-question/src/routes.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const router = new Router({
+    mode: 'history', //use browser history
+    routes: [
+        {
+            path: "/",
+            name: "Home",
+            component: () => import("./components/ListQuestionComponent")
+        },
+        {
+            path: "/questions",
+            name: "Questions",
+            component: () => import("./components/ListQuestionComponent")
+        },
+        {
+            path: "/questions/:id",
+            name: "Question Details",
+            component: () => import("./components/QuestionComponent")
+        }
+    ]
+});
+
+export default router;
