b90fa0177b706eb5e7f31a110e81370f07f603b1
Maciej Kowalik <m.kowalik@stat.gov.pl>
2019-12-16 13:32:28 +0100

    poprawa wyswietlania fiszek

--- frontend-interview-question/src/components/Home.vue
+++ frontend-interview-question/src/components/Home.vue
@@ -6,11 +6,11 @@
       
       <v-spacer class="mb-3"></v-spacer>
       <v-col
-        v-for="(question, i) in questions"
-        :key="i"
+        v-for="question in questions"
+        :key="question.id"
       >
         <v-card
-            max-width="400"
+            max-width="500"
             class="mx-auto"
         >
           <div class="question-content">
@@ -36,17 +36,51 @@
 
             <div class="action">
               <v-card-actions>
-                <v-btn text @click="checkAnswer(question.id)">Sprawdź odpowiedź</v-btn>
+                <v-flex>
+                  <v-btn text class="ma-2 px-0" @click="selectedQuestion = question">
+                    Sprawdź
+                  </v-btn>  
+                </v-flex>
+                <v-flex class="text-right">
+                  <v-btn text>umiem</v-btn>
+                  <v-btn text>nie umiem</v-btn>
+                </v-flex>
               </v-card-actions>
-            </div>
-
-            <div>
-              <span>Umiem</span>
-              <span>Nie umiem</span>
             </div>
           </div>
         </v-card>
       </v-col>
+
+      <v-dialog 
+        v-if="selectedQuestion"
+        v-model="selectedQuestion"
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            <span>{{ selectedQuestion.description }}</span>
+            <v-spacer></v-spacer>
+            <v-menu
+              bottom
+              left
+            >
+            </v-menu>
+          </v-card-title>
+          <v-card-text>
+            {{ selectedQuestion.answer }}
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              text
+              @click="selectedQuestion = false"
+            >
+              Zamknij
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
     </v-container>
   </div>
 </template>
@@ -60,7 +94,9 @@ export default {
     data() {
         return {
           questions: [],
-          color: ''
+          color: '',
+          dialog: false, 
+          selectedQuestion: null
         }
     },
     methods: {
@@ -70,10 +106,6 @@ export default {
                     this.questions = response.data;
                 });
         }, 
-        checkAnswer(id) {
-            this.$router.push(`/questions/${id}`);
-        }, 
-        
     },
     components: {
       MenuPanel,
@@ -95,9 +127,9 @@ export default {
   grid-template-areas: 
                       "t t t t"
                       "sb sb sb i"
-                      "a a a i"
+                      "a a a a"
 }
-.title{
+.title {
   grid-area: t;
 }
 .subtitle {
